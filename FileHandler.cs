84110228f7d271e9ba80a58357858ce68ec08726
Anton <antob.chebanitsa@gmail.com>
2021-01-12 19:18:06 +0300

    Add project files.

---
+++ FileHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Homework.ITAcademy6
+{
+    public class FileHandler
+    {
+        private const string PATH = "D:\\sample.txt";
+
+        public string FileReader()
+        {
+            var reader = new StreamReader(PATH);
+            return reader.ReadToEnd();
+        }
+
+        public string[] SplitIntoSentences()
+        {
+            var text = FileReader();
+            var separators = new char[] { '!', '.', '?' };
+            var sentences = text.Split(separators);
+            return sentences;
+        }
+
+        public string[] SplitIntoWords()
+        {
+            var text = FileReader();
+            var newText = Regex.Replace(text, "[-!.?(),\"\\-;:]", "");
+            var words = newText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+
+        public string[] SplitByPunctuationMarks()
+        {
+            var text = FileReader();
+            var separators = new char[] { '!', '.', '?', '(', ')', '"', '-', ';',':'};
+            var stringsSplitedByPunctuationMarks = text.Split(separators);
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Homework.ITAcademy6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new FileHandler();
+            
+        }
+    }
+}
